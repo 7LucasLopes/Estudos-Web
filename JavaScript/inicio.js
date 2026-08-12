9fa97156dc72c3367cb6c22b643d3e11936ac7fe
7LucasLopes <ld981506390@gmail.com>
2026-08-11 22:49:45 -0300

    aprendendo callBack function

--- JavaScript/inicio.js
+++ JavaScript/inicio.js
@@ -3,9 +3,22 @@
 }
 
 showMessage("Lucas")
-*/
+
 const showMessage1 = (username, email) => {
     console.log(`Olá, ${username}! Seu e-mail é: ${email} `)
 }
 
 showMessage1("Lucas", "godisgood@gamil.com")
+*/
+
+function execute(taskName,callBack){
+    console.log("Executando a tarefa: ", taskName)
+
+    callBack()
+}
+
+function callBack(){
+    console.log("Tarefa finalizada.")
+}
+
+execute("Download do arquivo...", callBack)

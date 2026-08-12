340e14392d412457659369c68bd715283d5724cd
7LucasLopes <ld981506390@gmail.com>
2026-08-11 22:39:22 -0300

    Inciando no JS. Aprendendo Arro wFunction

---
+++ JavaScript/inicio.js
@@ -0,0 +1,11 @@
+/*const showMessage = (name) => {
+    console.log("Olá, ", name)
+}
+
+showMessage("Lucas")
+*/
+const showMessage1 = (username, email) => {
+    console.log(`Olá, ${username}! Seu e-mail é: ${email} `)
+}
+
+showMessage1("Lucas", "godisgood@gamil.com")

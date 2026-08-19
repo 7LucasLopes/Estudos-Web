edd8c99f39d8dc03fe2ae13e2491ada76dca099a
Lucas Lopes <ld981506390@gmail.com>
2026-08-19 11:09:48 -0300

    keydown

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -1,4 +1,4 @@
-const ul = document.querySelector("ul")
+/*const ul = document.querySelector("ul")
 
 ul.addEventListener("scroll", (event) => {
     if (ul.scrollTop > 300){
@@ -9,4 +9,22 @@ ul.addEventListener("scroll", (event) => {
             behavior: "smooth",
         })
     }
+})
+
+
+const form = document.querySelector("form")
+
+form.onsubmit = (event) => {
+    event.preventDefault() //impedi o recarregamento da página quando é encaminhado um submit
+
+    console.log("Dados enviados sem recarregar a página!")
+}
+*/
+
+const input = document.querySelector("input")
+
+//keydown - quando uma tecla é pressionada (captura tudo)
+
+input.addEventListener("keydown", (event) => {
+    console.log(event.key)
 })
